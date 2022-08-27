2c26a0e5f32f8b7d02c143848ac7dc80c98fb1ae
Duc Nguyen <ducnrg@gmail.com>
2022-08-26 22:15:50 -0400

    add about components

--- src/component/mainPage/MainPage.js
+++ src/component/mainPage/MainPage.js
@@ -2,9 +2,12 @@
 
 import React from "react";
 import "./MainPage.css";
-import Navbar from "../header/NavBar.js";
+
 import { useInView } from "react-intersection-observer";
+//import components
 import Experience from "../experience/Experience";
+import About from "../about/About";
+import Navbar from "../header/NavBar.js";
 export default function MainPage() {
   const [sectionHome, sectionHomeInView] = useInView({ threshold: 0.5 });
   const [sectionAbout, sectionAboutInView] = useInView({ threshold: 0.5 });
@@ -32,35 +35,7 @@ export default function MainPage() {
         </div>
       </section>
       <section id="about" ref={sectionAbout}>
-        <div class="container reveal">
-          <h2>About</h2>
-          <div class="text-container">
-            <div class="text-box">
-              <h3>Section Text</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                eius molestiae perferendis eos provident vitae iste. Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quam blanditiis,
-                officiis, voluptate quisquam aperiam nobis est, quis neque esse
-                soluta dolores iste sit corrupti! Magnam, delectus qui. Odit,
-                accusamus doloremque.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex est
-                rerum id quis eaque rem suscipit reprehenderit quaerat vero
-                nesciunt?
-              </p>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Voluptatum, eaque voluptatibus. Eaque tempore quam nesciunt.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-                adipisci blanditiis assumenda atque officiis? Vel, fuga facilis
-                quia reprehenderit nihil optio harum. Quo sapiente veniam atque
-                corporis tempora non repellat?
-              </p>
-            </div>
-          </div>
-        </div>
+        <About />
       </section>
 
       <section id="academic" ref={sectionAcademic}>
